a91b940286a720d4e3ce6b4efe2568983ebdb8c3
YuneidyC <yuneidycastillo25@gmail.com>
2025-03-13 17:12:34 +0000

    Fill cells with random colors

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ function makeRows(rows, cols) {
 };
 
 makeRows(16, 16);
+
+container.addEventListener('mouseover', (e) => {
+    if (e.target.classList.contains('square'))
+    {
+        e.target.style.backgroundColor = getRandomColor();
+    }
+});
+
+function getRandomColor () {
+    let letters = '0123456789ABCDEF';
+    let color ='#';
+
+    for (let i = 0; i < 6; i++)
+    {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
